814d25259655f907bf1ee371b629e6c24abd9a8d
Zaim Zazali <zaim.zazali@gmail.com>
2022-05-09 23:58:50 +0800

    Update Open Deal

--- src/utils/IG.py
+++ src/utils/IG.py
@@ -40,3 +40,48 @@ class IG(metaclass=Singleton):
 
     def getIgAccountDetails(self, igService):
         return igService.create_session()
+
+    def getHistoricalPricesByDuration(self, igService, params):
+        return igService.fetch_historical_prices_by_epic_and_date_range(params['TARGET_EPIC'], 
+                                                                        params['RESOLUTION'], 
+                                                                        params['start_date'], 
+                                                                        params['end_date'])
+
+    def getHistoricalPricesByDataPoints(self, igService, params):
+        return igService.fetch_historical_prices_by_epic_and_num_points(params['TARGET_EPIC'], 
+                                                                        params['RESOLUTION'], 
+                                                                        params['DATA_POINTS'])
+
+    def getOpenPositions(self, igService):
+        return igService.fetch_open_positions()
+
+    def isAllowedToOpenDeal(self, igService, params):
+        openPositions = igService.fetch_open_positions()
+
+        if (params['deal_direction'] == 'BUY' and 
+            len(openPositions[openPositions['direction'] == 'BUY']) < params['LIMIT_BUY_COUNT']):
+            return True
+        elif (params['deal_direction'] == 'SELL' and 
+            len(openPositions[openPositions['direction'] == 'SELL']) < params['LIMIT_SELL_COUNT']):
+            return True
+
+        return False
+
+    def openDeal(self, igService, params):
+        igService.create_open_position(
+            currency_code           =params['currency_code'],
+            direction               =params['direction'],
+            epic                    =params['epic'],
+            order_type              =params['order_type'],
+            expiry                  =params['expiry'],
+            force_open              =params['force_open'],
+            guaranteed_stop         =params['guaranteed_stop'],
+            size                    =params['size'], 
+            level                   =params['level'],
+            limit_distance          =params['limit_distance'],
+            limit_level             =params['limit_level'],
+            quote_id                =params['quote_id'],
+            stop_level              =params['stop_level'],
+            stop_distance           =params['stop_distance'],
+            trailing_stop           =params['trailing_stop'],
+            trailing_stop_increment =params['trailing_stop_increment'])
