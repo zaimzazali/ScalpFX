6bdf2bade516f0789e523de9d28d9a71a5d9b051
Zaim Zazali <zaim.zazali@gmail.com>
2022-06-13 22:06:16 +0800

    Update data_ingestion_main.py

--- src/data_ingestion_main.py
+++ src/data_ingestion_main.py
@@ -11,12 +11,12 @@ def main():
     # Get historical data, then calculate the Mid values
     history = getHistoricalData(data['ig_service_live'], startDate)
     mid_df = calculateMidValues(history)
-    print(mid_df)
-    print(mid_df.values.tolist())
+    # print(mid_df)
+    # print(mid_df.values.tolist())
 
     # Delete dirty data, then push historical mid-value data to database
-    # deleteDirtyData(data['dbConfig'], startDate)
-    # pushDataToDatabase(data['dbConfig'], mid_df)
+    deleteDirtyData(data['dbConfig'], startDate)
+    pushDataToDatabase(data['dbConfig'], mid_df)
 
 
 if __name__ == "__main__":
